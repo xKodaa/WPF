e051063a18c01cd9d2d60be988f2717d62ca6f29
Jakub Kodytek <jakub.kodytek@email.cz>
2022-08-08 11:48:06 +0200

    2. Verze

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,91 +12,202 @@ namespace FormApp
     {
         private SqlDataAdapter dataAdapter = new SqlDataAdapter(); //komponenta, která přebírá data z databáze a může s nimi pracovat
         private SqlConnection connectToDatabase = new SqlConnection(); //instance pro připojování do databáze
+        private DataTable dataTable = new DataTable("Praxe_test"); //vytváření prostoru pro data
         private string connectionString = Connect.ConnString; //do proměnné je uložen string s parametry pro připojení do SQL databáze z třídy Connect.cs
+        private int age;
 
         public MainWindow()
         {
             InitializeComponent();
             this.WindowState = WindowState.Maximized; //fullscreen
+            //naplnění combo-boxu Stringy
+            optionComboBox.Items.Add("Search");
+            optionComboBox.Items.Add("Insert");
+            optionComboBox.Items.Add("Delete");
+            optionComboBox.SelectedIndex = 0; //nastavení výchozí hodnoty na nultej index (search)
+
+            //-----------------------------------------------------------------ZKOUŠKA PŘIPOJENÍ-------------------------------------------------------------//
             try
             {
                 connectToDatabase.ConnectionString = connectionString; //přiřazení connection stringu
-                SqlCommand fillDataQuery = new SqlCommand("SELECT * FROM dbo.HD_Stavy", connectToDatabase); //vyberu všechny data kterými poté plním formulář
-                fillForm(fillDataQuery);
-                fillComboBoxes();
+                connectToDatabase.Open(); //otevření databáze pro komunikaci
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString()); //vypíše error log
                 connectToDatabase.Close(); //zavře databázi
             }
-        }
-        //----------------------------------------------------------------ŘAZENÍ ZOBRAZENÝCH DAT----------------------------------------------------------------//
-        private void fillComboBoxes()
-        {
-
-            //do sorting combo boxu přidám jen 2 hodnoty:
-            comboBoxSorting.Items.Add("Vzestupně");
-            comboBoxSorting.Items.Add("Sestupně");
-
-            connectToDatabase.Open(); //otevírám databázi protože data pro combo box jsou názvy sloupců databáze
-            //dotaz co zobrazuje pouze názvy sloupců z tabulky
-            SqlCommand getNameOfColumnsQuery = new SqlCommand("SELECT name FROM sys.columns WHERE object_id = OBJECT_ID('dbo.HD_Stavy')", connectToDatabase);
-            SqlDataReader dataReader = getNameOfColumnsQuery.ExecuteReader(); //komponenta čte data získaná z dotazu
-
-            while (dataReader.Read()) //čte po řádcích
-            {
-                //listNazvu.Add(dataReader["name"].ToString());
-                comboBox.Items.Add(dataReader["name"].ToString()); //naplnění combo boxu názvy sloupců jako stringy
-
-            }
-            connectToDatabase.Close(); //po naplnění combo boxu s názvy sloupců už nepotřebuji otevřenou databázi
-        }
-        private void checkSort() //metoda pro zjištění jak se budou řadit data
-        {
-            connectToDatabase.Open();
-            string nazevSloupce = comboBox.Text; //do proměnné ukládám aktuální item jako text z combo boxu
-            if (comboBoxSorting.Text.Equals("Vzestupně"))
-            {
-                SqlCommand sortTableQuery = new SqlCommand("SELECT * FROM dbo.HD_Stavy ORDER BY " + nazevSloupce + " ASC", connectToDatabase);
-                fillForm(sortTableQuery);
-            }
-            else
-            {
-                SqlCommand sortTableQuery = new SqlCommand("SELECT * FROM dbo.HD_Stavy ORDER BY " + nazevSloupce + " DESC", connectToDatabase);
-                fillForm(sortTableQuery);
-            }
+            //----------------------------------------------------------------/ZKOUŠKA PŘIPOJENÍ-------------------------------------------------------------//
             connectToDatabase.Close();
-
+            refresh(); //chci prvně naplnit formulář daty, ale později jsem si na to udělal metodu, takže si ji tady rovnou volám
         }
-
-        private void fillForm(SqlCommand query) //metoda pro naplnění formuláře daty
-        {
-            DataTable dataTable = new DataTable("Stavy"); //vytvářím prostor pro data
-            dataAdapter = new SqlDataAdapter(query); //do data adapteru posílám dotaz na výběr určitých dat
-            dataAdapter.Fill(dataTable); //data adapter zpracovanými daty naplní data table "Stavy"
-            dataGrid.DataContext = dataTable; //data table se promítne do formuláře
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) { checkSort(); }
-
-        //---------------------------------------------------------------/ŘAZENÍ ZOBRAZENÝCH DAT-------------------------------------------------------------//
 
 
         //-----------------------------------------------------------------REAL TIME SEARCHING---------------------------------------------------------------//
         private void searchText(object sender, TextChangedEventArgs e)
         {
-            connectToDatabase.Open();
-            SqlCommand searchQuery = new SqlCommand("SELECT * FROM dbo.HD_Stavy WHERE Nazev LIKE '" + textBoxNazev.Text + "%'", connectToDatabase);
-            dataAdapter = new SqlDataAdapter(searchQuery);
-            DataTable dataTable = new DataTable("Stavy");
-            dataAdapter.Fill(dataTable);
-            dataGrid.DataContext = dataTable;
-            connectToDatabase.Close();
+           if (optionComboBox.Text.Equals("Search")) //event se spouští použe pokud je v comboboxu hodnota "Search"
+           {
+                if (textBoxName.IsSelectionActive) //hledání podle text-boxu Name
+                {
+                    SqlCommand searchQuery = new SqlCommand("SELECT * FROM dbo.Praxe_test WHERE Name LIKE '" + textBoxName.Text + "%'", connectToDatabase);
+                    fillForm(searchQuery);
+                }
+                if (textBoxSurname.IsSelectionActive) //hledání podle text-boxu Surname
+                {
+                    SqlCommand searchQuery = new SqlCommand("SELECT * FROM dbo.Praxe_test WHERE Surname LIKE '" + textBoxSurname.Text + "%'", connectToDatabase);
+                    fillForm(searchQuery);
+                }
+                if (textBoxAge.IsSelectionActive) //hledání podle text-boxu Age
+                {
+                    SqlCommand searchQuery = new SqlCommand("SELECT * FROM dbo.Praxe_test WHERE Age LIKE '" + textBoxAge.Text + "%'", connectToDatabase);
+                    fillForm(searchQuery);
+                }
+           }
+
         }
         //----------------------------------------------------------------/REAL TIME SEARCHING---------------------------------------------------------------//
 
 
+        //---------------------------------------------------------------METODY PRO PRÁCI S DATY-------------------------------------------------------------//
+        private void insertData() //metoda která vkládá data do databáze
+        {
+            connectToDatabase.Open();
+            try
+            {
+                age = Int32.Parse(textBoxAge.Text);
+            } catch (FormatException ex)
+            {
+                MessageBox.Show("Prosím zadávejte validní hodnoty! [-Věk musí být číslo]", "Input format Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                connectToDatabase.Close(); //nechci aby se mi do databáze cokoliv vložilo pokud je špatný input
+                return; //aby se neprovedl zbytek kódu v metodě
+            }
+
+            if (!textBoxName.Text.Equals("") && !textBoxSurname.Text.Equals("")) //pokud Name a Surname není prázdný String
+            {
+                SqlCommand insertQuery = new SqlCommand("INSERT INTO dbo.Praxe_test VALUES('" + textBoxName.Text + "','" + textBoxSurname.Text + "'," +
+                   age + ")", connectToDatabase);
+                int sucessful = insertQuery.ExecuteNonQuery();
+
+                if (sucessful < 0 || sucessful == 0) //ověřovací if
+                {
+                    MessageBox.Show("Záznam se nepodařilo vložit", "Task failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Záznam se úspěšně vložil", "Task successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vyplňte prosím všechna data!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            connectToDatabase.Close();
+        }
+
+        private void deleteData() //metoda která maže data z databáze
+        {
+            connectToDatabase.Open();
+            try
+            {
+                age = Int32.Parse(textBoxAge.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Prosím zadávejte validní hodnoty! [-Věk musí být číslo]", "Input format Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                connectToDatabase.Close(); //nechci aby se mi do databáze cokoliv vložilo pokud je špatný input
+                return; //aby se neprovedl zbytek kódu v metodě
+            }
+
+            if (!textBoxName.Text.Equals("") && !textBoxSurname.Text.Equals("")) //pokud Name a Surname není prázdný String
+            {
+                SqlCommand deleteQuery = new SqlCommand("DELETE FROM dbo.Praxe_test WHERE Name LIKE '" + textBoxName.Text + "' AND Surname LIKE '" + textBoxSurname.Text
+                  + "' AND Age = " + age, connectToDatabase);
+                int sucessful = deleteQuery.ExecuteNonQuery();
+
+                if (sucessful < 0 || sucessful == 0) //ověřovací if
+                {
+                    MessageBox.Show("Záznam se nepodařilo smazat", "Task failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Záznam se úspěšně smazal", "Task successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vyplňte prosím všechna data!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            connectToDatabase.Close();
+        }
+        //--------------------------------------------------------------/METODY PRO PRÁCI S DATY-------------------------------------------------------------//
+
+        //-------------------------------------------------------------------POMOCNÉ METODY------------------------------------------------------------------//
+        private void fillForm(SqlCommand query) //metoda pro naplnění formuláře daty
+        {
+            connectToDatabase.Open();
+            dataTable = new DataTable("Praxe_test"); //vytvářím prostor pro data
+            dataAdapter = new SqlDataAdapter(query); //do data adapteru posílám dotaz na výběr určitých dat
+            dataAdapter.Fill(dataTable); //data adapter zpracovanými daty naplní data table "Praxe_test"
+            dataGrid.DataContext = dataTable; //data table se promítne do formuláře
+            connectToDatabase.Close();
+
+        }
+
+        private void refresh() //metoda která refreshne tabulku a zobrazí všechna data
+        {
+            SqlCommand refreshTable = new SqlCommand("SELECT * FROM dbo.Praxe_test", connectToDatabase);
+            fillForm(refreshTable);
+        }
+
+        
+        private void buttonClick(object sender, RoutedEventArgs e) //metoda která čeká na event po kliknutí na button
+        {
+            //button volá funkce podle itemu, který je vybrán v combo-boxu
+            if (optionComboBox.SelectedIndex == 0) //search
+            {   
+                clearTextBoxes();
+                refresh();
+            }
+            if (optionComboBox.SelectedIndex == 1) //insert
+            {
+                insertData();
+                clearTextBoxes();
+                refresh();
+            }
+            if (optionComboBox.SelectedIndex == 2) //delete
+            {
+                deleteData();
+                clearTextBoxes();
+                refresh();
+            }
+        }
+
+        private void changeTextOfButton(object sender, SelectionChangedEventArgs e) //metoda která mění text tlačítka podle vybraného itemu v combo-boxu
+        {
+            if (optionComboBox.SelectedIndex == 0) //search
+            {
+                buttonOption.Content = "Clear";
+            }
+            if (optionComboBox.SelectedIndex == 1) //insert
+            {
+                buttonOption.Content = "Insert";
+            }
+            if (optionComboBox.SelectedIndex == 2) //delete
+            {
+                buttonOption.Content = "Delete";
+            }
+        }
+
+        private void clearTextBoxes() //metoda která vymaže text z text-boxů
+        {
+            textBoxName.Text = "";
+            textBoxSurname.Text = "";
+            textBoxAge.Text = "";
+        }
+        //------------------------------------------------------------------/POMOCNÉ METODY------------------------------------------------------------------//
     }
 
 }
